6e6495fa32778c39bf4da963ff69dc37ed55f7b4
mik <mikail.aziev@gmail.com>
2018-08-27 14:51:52 +0300

    install react

---
+++ resources/views/main.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Furqan</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+    <div id="app"></div>
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
